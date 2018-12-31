e02cdc411e7bcee4db961b666a3809b8d2b1ddb8
PHUGIANG <phugiang123@gmail.com>
2018-12-31 21:26:00 +0700

    create example parallax background

---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour {
+
+	public float Speed = 0.2f;
+	private MeshRenderer render;
+	private Vector2 offsetDefaul;
+	// Use this for initialization
+	void Start () {
+		render = GetComponent<MeshRenderer>();
+		offsetDefaul = render.sharedMaterial.GetTextureOffset("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Mathf.Repeat(Time.time * Speed, 1);
+		Vector2 newOffset = new Vector2(x, offsetDefaul.y);
+		render.sharedMaterial.SetTextureOffset("_MainTex", newOffset);
+	}
+}
